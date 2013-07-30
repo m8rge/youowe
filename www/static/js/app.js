bcc5ee01684db5193def5f0edb0517020402376e
m8rge <to.merge@gmail.com>
2013-07-30 16:37:00 +0600

    jqtjs

--- www/static/js/app.js
+++ www/static/js/app.js
@@ -1,11 +1,32 @@
-function YouOweApp($scope) {
+var app = angular.module("YouOweApp", []);
+
+app.controller("AppController", function($scope, $http) {
     $scope.currency = 'р.';
-    $scope.oweYou = [
-        { email: 'to.merge@gmail.com', totalSum: 420 },
-        { email: 'ramm@66.ru', totalSum: 30 }
-    ];
-    $scope.youOwe = [
-        { email: 'to.merge@gmail.com', totalSum: 420 },
-        { email: 'ramm@66.ru', totalSum: 30 }
-    ];
-}
+
+    if (window.location.hash != '#register') {
+        $http.get('v1/users.json').success(function (data) {
+            $scope.authorized = true;
+            $scope.users = data;
+        }).error(function () {
+            jqtouch.goTo('#register');
+            $scope.authorized = false;
+        });
+//    $http.get('v1/debts/oweyou.json').success(function(data) {
+//        $scope.oweYou = data;
+//    });
+//
+//    $http.get('v1/debts/youowe.json').success(function(data) {
+//        $scope.youOwe = data;
+//    });
+    }
+});
+
+app.controller("RegisterController", function($scope, $http) {
+    $scope.register = function () {
+        $http.post('v1/users', $scope.user).success(function () {
+            alert('success!');
+        }).error(function(){
+            alert('error');
+        });
+    };
+});
