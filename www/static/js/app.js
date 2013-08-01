48256d4c239820136db1a6a5f6f4ad60d32124a0
m8rge <to.merge@gmail.com>
2013-08-01 21:30:24 +0600

    user model

--- www/static/js/app.js
+++ www/static/js/app.js
@@ -1,63 +1,84 @@
 var app = angular.module("YouOweApp", []);
 
-app.controller("AppController", function($scope, $http, $location) {
-    $scope.currency = 'р.';
+app.factory('User', function ($http, $rootScope, $location) {
+    var user = {
+        loggedIn: false,
+        users: [],
+        oweYou: [],
+        youOwe: []
+    };
 
-    var loadData = function () {
+    user.login = function () {
+        var that = this;
         $http.get('v1/users.json').success(function (data) {
-            $scope.$emit('authSucceed');
-            $scope.authorized = true;
-            $scope.users = data;
-        }).error(function () {
-            jqtouch.goTo('#register');
-            $scope.authorized = false;
-        });
-        $scope.$on('authSucceed', function() {
-            $http.get('v1/debts/oweyou.json').success(function(data) {
-                $scope.oweYou = data;
-            });
+            that.loggedIn = true;
+            $rootScope.$broadcast('loginSuccess');
+            that.users = data;
 
-            $http.get('v1/debts/youowe.json').success(function(data) {
-                $scope.youOwe = data;
+            $http.get('v1/debts/oweyou.json').success(function (data) {
+                that.oweYou = data;
             });
+            $http.get('v1/debts/youowe.json').success(function (data) {
+                that.youOwe = data;
+            });
+        }).error(function () {
+            that.loggedIn = false;
+            $rootScope.$broadcast('loginFail');
         });
     };
 
-    $scope.$on('tryLogin', loadData);
-    $scope.$on('logout', function () {
-        $scope.authorized = false;
-        $scope.users = [];
-        $scope.oweYou = [];
-        $scope.youOwe = [];
-    });
+    user.logout = function () {
+        var that = this;
+        this.loggedIn = false;
+        this.users = [];
+        this.oweYou = [];
+        this.youOwe = [];
 
-    if (window.location.hash != '#register') {
-        loadData();
+        $http.post($location.protocol() + '://::@' + $location.host() + ':' + $location.port() + '/v1/logout').success(function () {
+            that.login();
+        }).error(function () {
+            $rootScope.$broadcast('loginFail');
+        });
+    };
+
+    return user;
+});
+
+app.controller("AppController", function ($scope, $http, $window, User) {
+    $scope.currency = 'р.';
+    $scope.user = User;
+
+    if ($window.location.hash != '#register') {
+        User.login();
     }
 
     $scope.logout = function () {
-        $scope.$emit('logout');
-        $http.post($location.protocol() + '://::@'+$location.host()+':'+$location.port()+'/v1/logout').success(function() {
-            $scope.$emit('tryLogin');
-        }).error(function () {
-            jqtouch.goTo('#register');
-        });
+        User.logout();
     }
 });
 
-app.controller("RegisterController", function($scope, $http) {
-    $scope.register = function () {
-        $http.post('v1/users', $scope.user).success(function (data, status) {
-            if (status == 201) {
-                alert('success!');
-            } else {
-                alert('error');
-            }
-        });
-    };
+app.controller("RegisterController", function ($scope, $http, $location, User) {
+    /*
+     $scope.register = function () {
+     $http.post('v1/users', $scope.user).success(function (data, status) {
+     if (status == 201) {
+     alert('success!');
+     } else {
+     alert('error');
+     }
+     });
+     };
+     */
+
+    $scope.$on('loginFail', function () {
+        jqtouch.goTo('#register');
+    });
+
+    $scope.$on('loginSuccess', function () {
+        jqtouch.goTo('#home');
+    });
 
     $scope.login = function () {
-        jqtouch.goTo('#home');
-        $scope.$emit('tryLogin');
-    }
+        User.login();
+    };
 });
