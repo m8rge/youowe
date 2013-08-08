04b3d25ce96a3a74c8c6841934e45f6db50ce818
m8rge <to.merge@gmail.com>
2013-08-08 10:51:26 +0600

    refactor, prepare to adddebt

--- www/static/js/app.js
+++ www/static/js/app.js
@@ -1,41 +1,61 @@
 var app = angular.module("YouOweApp", []);
 
+app.factory('Settings', function(){
+    return {
+        currency: 'р.'
+    }
+});
+
+app.factory('Users', function($http, $rootScope) {
+    var users = {
+        list: []
+    };
+
+    $rootScope.$on('login', function () {
+        $http.get('v1/users.json').success(function (data) {
+            $rootScope.$broadcast('loginSuccess');
+            users.list = data;
+        }).error(function () {
+            $rootScope.$broadcast('loginFail');
+        });
+    });
+
+    $rootScope.$on('logout', function () {
+        users.list = [];
+    });
+
+    return users;
+});
+
 app.factory('User', function ($http, $rootScope, $location) {
     var user = {
         loggedIn: false,
-        users: [],
         oweYou: [],
         youOwe: []
     };
 
     user.login = function () {
-        var that = this;
-        $http.get('v1/users.json').success(function (data) {
-            that.loggedIn = true;
-            $rootScope.$broadcast('loginSuccess');
-            that.users = data;
-
-            $http.get('v1/debts/oweyou.json').success(function (data) {
-                that.oweYou = data;
-            });
-            $http.get('v1/debts/youowe.json').success(function (data) {
-                that.youOwe = data;
-            });
-        }).error(function () {
-            that.loggedIn = false;
-            $rootScope.$broadcast('loginFail');
-        });
+        $rootScope.$broadcast('login');
     };
 
+    $rootScope.$on('loginSuccess', function() {
+        user.loggedIn = true;
+        $http.get('v1/debts/oweyou.json').success(function (data) {
+            user.oweYou = data;
+        });
+        $http.get('v1/debts/youowe.json').success(function (data) {
+            user.youOwe = data;
+        });
+    });
+
     user.logout = function () {
-        var that = this;
+        $rootScope.$broadcast('logout');
         this.loggedIn = false;
-        this.users = [];
         this.oweYou = [];
         this.youOwe = [];
 
         $http.post($location.protocol() + '://::@' + $location.host() + ':' + $location.port() + '/v1/logout').success(function () {
-            that.login();
+            user.login();
         }).error(function () {
             $rootScope.$broadcast('loginFail');
         });
@@ -44,20 +64,22 @@ app.factory('User', function ($http, $rootScope, $location) {
     return user;
 });
 
-app.controller("AppController", function ($scope, $http, $window, User) {
-    $scope.currency = 'р.';
+/****************
+ * CONTROLLERS
+ */
+app.controller("AppController", function ($scope, $window, Users, User, Settings) {
+    $scope.settings = Settings;
     $scope.user = User;
+    $scope.users = Users;
 
     if ($window.location.hash != '#register') {
         User.login();
     }
-
-    $scope.logout = function () {
-        User.logout();
-    }
 });
 
-app.controller("RegisterController", function ($scope, $http, $location, User) {
+app.controller("RegisterController", function ($scope, $http, User) {
+    $scope.user = User;
+
     $scope.register = function () {
         if (!$scope.RegisterForm.$valid) {
             alert('Пожалуйста исправьте ошибки и попробуйте зарегистрироваться заново');
@@ -84,8 +106,22 @@ app.controller("RegisterController", function ($scope, $http, $location, User) {
     $scope.$on('loginSuccess', function () {
         jqtouch.goTo('#home');
     });
+});
 
-    $scope.login = function () {
-        User.login();
+app.controller("AddDebtController", function($scope, Settings) {
+   $scope.settings = Settings;
+
+    $scope.addDebt = function () {
+        if (!$scope.AddDebtForm.$valid) {
+            alert('Пожалуйста исправьте ошибки и попробуйте заново');
+        } else {
+            $http.post('v1/users', $scope.user).success(function (data, status) {
+                if (status == 201) {
+                    alert('Теперь Вы можете войти под своими реквизитами');
+                } else {
+                    alert('error: '. data);
+                }
+            });
+        }
     };
 });
